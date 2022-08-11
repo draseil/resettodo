f1af3ac9cc0027d01c7ff7be7a80730a8551aa3d
Sariana <sariana.martinesi@gmail.com>
2022-08-12 04:00:42 +0800

    renamed messages to checkboxes, changed POST method to GET

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '103d485a680c3850328fdcdecc8c32b752357d80b19d303d'
 
-messages = []
+checkboxes = []
 
 @app.route('/', methods=('GET', 'POST'))
 def create():
-	if request.method == 'POST':
-		todo_list = request.form['todo-list']
+    if request.method == 'GET':
+        todo_list = request.args.get('todo-list')
 
-		if not todo_list:
-			flash('please fill out the todo list')
-		else:
-			for line in todo_list.splitlines():
-				messages.append(line)
-			return redirect(url_for('main'))
-	
-	return render_template('index.html')
+        if not todo_list:
+            flash('please fill out the todo list')
+        else:
+            checkboxes.clear();
+            for line in todo_list.splitlines():
+                checkboxes.append(line)
+            return render_template('generated.html', len = len(checkboxes), checkboxes = checkboxes)
+    
+    return render_template('index.html')
 
 @app.route('/generated', methods=('GET', 'POST'))
 def main():
-	return render_template('generated.html', len = len(messages), messages = messages)
+    return render_template('generated.html', len = len(checkboxes), checkboxes = checkboxes)
