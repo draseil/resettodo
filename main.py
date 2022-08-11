ae62b7dca0d70b7ef4ce9d700831d491f29df4b2
Sariana <sariana.martinesi@gmail.com>
2022-08-12 02:31:43 +0800

    added basic functionality for forms

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '103d485a680c3850328fdcdecc8c32b752357d80b19d303d'
 
-@app.route('/')
+messages = []
+
+@app.route('/', methods=('GET', 'POST'))
+def create():
+	if request.method == 'POST':
+		todo_list = request.form['todo-list']
+
+		if not todo_list:
+			flash('please fill out the todo list')
+		else:
+			messages.append({'title': 'Message', 'content': todo_list})
+			return redirect(url_for('main'))
+	
+	return render_template('index.html')
+
+@app.route('/generated', methods=('GET', 'POST'))
 def main():
-    return render_template('index.html')
+	return render_template('generated.html', messages=messages)
